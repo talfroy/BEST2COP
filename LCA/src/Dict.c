996fc3d04c39611c75c3170b148b4c354a92a015
Jro Luttringer <jro.luttringer@gmail.com>
2022-05-09 14:42:00 +0200

    Adding LCA

---
+++ LCA/src/Dict.c
@@ -0,0 +1,51 @@
+#include "../include/Dict.h"
+
+
+
+void Dict_init(Dict_t* dic, int size, int msd)
+{
+    dic->paths = malloc(size * sizeof(int));
+    ASSERT(dic->paths, , size);
+
+    dic->preds = malloc(size * sizeof(short));
+    ASSERT(dic->preds, , size);
+    
+    for (int i = 0 ; i < size ; i++) {
+        dic->paths[i] = INF;
+    }
+    dic->max_m1 = size;
+    dic->max_m0 = msd;
+    dic->nb_add = 0;
+}
+
+
+void Dict_add(Dict_t* dic, my_m0 key_seg, my_m1 key_cost, my_m2 value, int currDag, int lastSegment, short pred)
+{
+    if (key_seg < dic->max_m0 && key_cost < dic->max_m1) {
+        dic->paths[key_seg][key_cost].m2 = value;
+        dic->paths[key_seg][key_cost].m1 = key_cost; // TODO: UNNECESSARY ?
+        dic->paths[key_seg][key_cost].m0 = key_seg; // TODO: UNNECESSARY ?
+        dic->paths[key_seg][key_cost].currDag = currDag;
+        dic->paths[key_seg][key_cost].lastSegment = lastSegment;
+        dic->preds[key_seg][key_cost] = pred;
+    }
+}
+
+
+void Dict_print(Dict_t* dic)
+{
+    printf("Dict : ");
+    for (my_m1 i = 0 ; i < MAX_SIZE ; i++) {
+        if (dic->paths[i] != INF) {
+            printf("(%d ; %d) ", i, dic->paths[i]);
+        }
+    }
+    printf("\n\n\n");
+}
+
+
+void Dict_free(Dict_t* dic)
+{
+    free(dic->paths);
+    free(dic->preds);
+}
---
+++ LCA/src/Pfront.c
@@ -0,0 +1,106 @@
+#include "../include/Pfront.h"
+
+
+int cmp(const void* a, const void* b)
+{
+    const int* pa = a;
+    const int* pb = b;
+    return *pa - *pb;
+}
+
+
+void Pfront_init(Pfront_t* bp, int size)
+{
+    bp->heapSize = 0;
+    bp->keys = malloc(size * sizeof(my_m1));
+    ASSERT(bp->keys, , size);
+    bp->maxSize = size;
+}
+
+
+void Pfront_insert_key(Pfront_t* bp, my_m1 key)
+{
+    if (bp->heapSize < bp->maxSize) {
+        bp->keys[bp->heapSize] = key;
+        bp->heapSize++;
+    }
+}
+
+
+void Pfront_sort(Pfront_t* bp)
+{
+    qsort(bp->keys, bp->heapSize, sizeof(my_m1), cmp);
+}
+
+
+int Pfront_union(Pfront_t* pfront, Pfront_t* pfcand)
+{
+    if (pfront->heapSize + pfcand->heapSize > pfront->maxSize) {
+        return pfront->maxSize;
+    } else {
+        return pfront->heapSize + pfcand->heapSize;
+    }
+}
+
+
+void Pfront_print(Pfront_t* pf, FILE* out)
+{
+    for (int i = 0 ; i < pf->heapSize ; i++) {
+        fprintf(out, " %d ", pf->keys[i]);
+    }
+    printf("\n");
+}
+
+Pfront_t* Pfront_merge_sort(Pfront_t* pfront, Pfront_t* pfcand)
+{
+    Pfront_t* ret = malloc(sizeof(Pfront_t));
+    Pfront_init(ret, pfront->maxSize);
+
+    int pfrontIndex = 0;
+    int pfcandIndex = 0;
+
+    while (pfrontIndex < pfront->heapSize && pfcandIndex < pfcand->heapSize) {
+        if (pfront->keys[pfrontIndex] < pfcand->keys[pfcandIndex]) {
+            Pfront_insert_key(ret, pfront->keys[pfrontIndex]);
+            pfrontIndex++;
+        } else if (pfront->keys[pfrontIndex] > pfcand->keys[pfcandIndex]) {
+            Pfront_insert_key(ret, pfcand->keys[pfcandIndex]);
+            pfcandIndex++;
+        } else {
+            Pfront_insert_key(ret, pfcand->keys[pfcandIndex]);
+            pfcandIndex++;
+            pfrontIndex++;
+        }
+    }
+
+
+    while (pfrontIndex < pfront->heapSize) {
+        Pfront_insert_key(ret, pfront->keys[pfrontIndex]);
+        pfrontIndex++;
+    }
+
+
+    while (pfcandIndex < pfcand->heapSize) {
+        Pfront_insert_key(ret, pfcand->keys[pfcandIndex]);
+        pfcandIndex++;
+    }
+
+    return ret;
+}
+
+void Pfront_free(Pfront_t* bh)
+{
+    free(bh->keys);
+}
+
+
+bool hasapath(Pfront_t** pfront, int dst)
+{
+    for (int i = 0 ; i <= SEG_MAX ; i++) {
+        if (pfront[i][dst].heapSize != 0) {
+            return true;
+        }
+    }
+
+    return false;
+}
